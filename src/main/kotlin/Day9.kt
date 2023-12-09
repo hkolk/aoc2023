5ea0756b70228d899ef8cc553c229bea8901f46c
Harm Kolk <github@sunstroke.nl>
2023-12-09 09:37:46 +0100

    Day9

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,25 +1,16 @@
 class Day9( input: List<String>) {
 
     val input = input.map { it.splitIgnoreEmpty(" ").map(String::toInt) }
-    private fun findNextDiff(nums: List<Int>): Int {
+    
+    private fun findDiff(nums: List<Int>, funct:(List<Int>, Int ) -> Int): Int {
         val diffs = nums.windowed(2).map { it[1] - it[0] }
         return if(diffs.all { it == 0 }) {
             0
         } else {
-            diffs.last() + findNextDiff(diffs)
+            funct(diffs, findDiff(diffs, funct))
         }
-
     }
+    fun solvePart1() = input.sumOf { it.last() + findDiff(it) { nums, diff -> nums.last() + diff } }
+    fun solvePart2() = input.sumOf { it.first() - findDiff(it) { nums, diff -> nums.first() - diff } }
 
-    private fun findPrevDiff(nums: List<Int>): Int {
-        val diffs = nums.windowed(2).map { it[1] - it[0] }
-        return if(diffs.all { it == 0 }) {
-            0
-        } else {
-            diffs.first() - findPrevDiff(diffs)
-        }
-
-    }
-    fun solvePart1() = input.sumOf { it.last()+findNextDiff(it) }
-    fun solvePart2() = input.sumOf { it.first()-findPrevDiff(it) }
 }
