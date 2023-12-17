7a4447939c680b6967ad215d1b4b97c57442138f
Harm Kolk <github@sunstroke.nl>
2023-12-17 10:24:29 +0100

    Day14 speed improvement

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,12 +1,14 @@
 class Day14(val input: List<String>) {
     val map = input.flatMapIndexed{y, line -> line.mapIndexed{x, c -> Point2D(x, y) to c}.filter { it.second != '.' }}.toMap()
-    val yRange = 0..< input.size
-    val xRange = 0..< input.first().length
+    val blocks = map.filter { it.value == '#' }.map { it.key }.toSet()
+    val yMax = input.size -1
+    val xMax = input.first().length -1
     val directions = mapOf("NORTH" to Point2D.NORTH, "WEST" to Point2D.WEST, "SOUTH" to Point2D.SOUTH, "EAST" to Point2D.EAST)
 
     fun roll(startMap: Map<Point2D, Char>, dir: String): Pair<Map<Point2D, Char>, Int> {
         val direction = directions[dir]!!
-        val newMap = map.filter { it.value == '#' }.toMutableMap()
+        val newRoundRocks = mutableSetOf<Point2D>()
+        val newMap = blocks.associateWith { '#' }.toMutableMap()
         var changes = 0
         var roundRocks = startMap.filter { it.value == 'O' }.keys.sortedBy { if(dir in listOf("WEST", "EAST")) it.x else it.y }
         if(dir in listOf("SOUTH", "EAST")) {
@@ -14,19 +16,24 @@ class Day14(val input: List<String>) {
         }
         roundRocks.forEach { pos ->
             val newPoint = direction(pos).let {
-                Point2D(x=it.x.coerceIn(xRange), y=it.y.coerceIn(yRange))
+                val x = it.x.coerceIn(0, xMax)
+                val y = it.y.coerceIn(0, yMax)
+                Point2D(x=x, y=y)
             }
-            if (newMap.contains(newPoint)) {
+            if (blocks.contains(newPoint) || newMap.contains(newPoint)) {
                 // spot is taken already
+                //newRoundRocks.add(pos)
                 newMap[pos] = 'O'
             } else {
                 // roll north
+                //newRoundRocks.add(newPoint)
                 newMap[newPoint] = 'O'
                 if(newPoint != pos) {
                     changes += 1
                 }
             }
         }
+        //return (blocks.associateWith{ '#' } + newRoundRocks.associateWith {'O'}) to changes
         return newMap to changes
     }
 
@@ -59,7 +66,6 @@ class Day14(val input: List<String>) {
                 val prevRound = cache.indexOf(newMap)
                 val cycle = step - prevRound
                 val idx = prevRound + (1_000_000_000-1 - prevRound) % cycle
-                println("idx: $idx")
                 val maxY = newMap.maxOf { it.key.y }+1
                 return cache[idx].filter { it.value == 'O' }.keys.sumOf { maxY - it.y }
             } else {
